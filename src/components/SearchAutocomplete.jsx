d67018c50539245e502a30cd1fb5ec36c89d5bd2
ONG TEE SENG, JEREMY _ <jeremyong.2014@smu.edu.sg>
2020-01-04 01:55:13 +0800

    store tickerList in client instead of retrieving from server

--- src/components/SearchAutocomplete.jsx
+++ src/components/SearchAutocomplete.jsx
@@ -13,6 +13,8 @@ import * as _ from "lodash";
 import gql from 'graphql-tag';
 import { Popper, Paper, MenuItem } from '@material-ui/core';
 
+import tickers from "../tickerList.json";
+
 const styles = theme => ({
     inputRoot: {
         color: 'inherit',
@@ -72,117 +74,113 @@ class SearchAutoComplete extends Component {
     render() {
         const { classes } = this.props;
         return (
-            <Query query={LOAD_TICKERS}>
-                {({ loading, error, data: { tickers } }) => (
-                    <ApolloConsumer>
-                        {client => {
-                            if (!tickers) return null;
-                            // console.log('tickers', tickers);
 
-                            return (
-                                <Autosuggest
-                                    focusInputOnSuggestionClick={false}
-                                    onSuggestionsFetchRequested={({ value, reason }) => {
-                                        const suggestions = _.chain(tickers)
-                                            .map(ticker => ({
-                                                value: ticker.Ticker,
-                                                label: ticker.Name,
-                                            }))
-                                            .filter(ticker => {
-                                                const toCompare = (ticker.label + ticker.value).trim().toLowerCase();
-                                                return toCompare.includes(value.trim().toLowerCase())
-                                            })
-                                            .value()
-                                            .slice(0, 5)
-                                        this.setState({ suggestions })
-                                    }}
-                                    onSuggestionsClearRequested={() => { this.setState({ suggestions: [] }) }}
-                                    onSuggestionSelected={(event, { suggestion }) => {
-                                        client.writeData({ data: { tickerName: suggestion.value } })
-                                    }}
-                                    getSuggestionValue={(suggestion) => suggestion.label}
-                                    suggestions={this.state.suggestions}
-                                    inputProps={{
-                                        // value: this.state.selectedValue,
-                                        // onChange: (event) => {
-                                        //     const selectedValue = event.target.value;
-                                        //     this.setState({ selectedValue });
-                                        // },
-                                        classes,
-                                        label: 'Label',
-                                        placeholder: 'Search...',
-                                        value: this.state.popper,
-                                        onChange: this.handleChange('popper'),
-                                        inputRef: node => {
-                                            this.popperNode = node;
-                                        },
-                                    }}
-                                    renderSuggestion={(suggestion, { query, isHighlighted }) => {
-                                        const matches = match(suggestion.label, query);
-                                        const parts = parse(suggestion.label, matches);
+            <ApolloConsumer>
+                {client => {
+                    if (!tickers) return null;
+                    return (
+                        <Autosuggest
+                            focusInputOnSuggestionClick={false}
+                            onSuggestionsFetchRequested={({ value, reason }) => {
+                                const suggestions = _.chain(tickers)
+                                    .map(ticker => ({
+                                        value: ticker.Ticker,
+                                        label: ticker.Name,
+                                    }))
+                                    .filter(ticker => {
+                                        const toCompare = (ticker.label + ticker.value).trim().toLowerCase();
+                                        return toCompare.includes(value.trim().toLowerCase())
+                                    })
+                                    .value()
+                                    .slice(0, 5)
+                                this.setState({ suggestions })
+                            }}
+                            onSuggestionsClearRequested={() => { this.setState({ suggestions: [] }) }}
+                            onSuggestionSelected={(event, { suggestion }) => {
+                                client.writeData({ data: { tickerName: suggestion.value } })
+                            }}
+                            getSuggestionValue={(suggestion) => suggestion.label}
+                            suggestions={this.state.suggestions}
+                            inputProps={{
+                                // value: this.state.selectedValue,
+                                // onChange: (event) => {
+                                //     const selectedValue = event.target.value;
+                                //     this.setState({ selectedValue });
+                                // },
+                                classes,
+                                label: 'Label',
+                                placeholder: 'Search...',
+                                value: this.state.popper,
+                                onChange: this.handleChange('popper'),
+                                inputRef: node => {
+                                    this.popperNode = node;
+                                },
+                            }}
+                            renderSuggestion={(suggestion, { query, isHighlighted }) => {
+                                const matches = match(suggestion.label, query);
+                                const parts = parse(suggestion.label, matches);
 
-                                        return (
-                                            <MenuItem selected={isHighlighted} component="div">
-                                                <div>
-                                                    {parts.map((part, index) =>
-                                                        part.highlight ? (
-                                                            <span key={String(index)} style={{ fontWeight: 500 }}>
-                                                                {part.text}
-                                                            </span>
-                                                        ) : (
-                                                                <strong key={String(index)} style={{ fontWeight: 300 }}>
-                                                                    {part.text}
-                                                                </strong>
-                                                            ),
-                                                    )}
-                                                </div>
-                                            </MenuItem>
-                                        );
-                                    }}
-                                    renderInputComponent={(inputProps) => (
-                                        <InputBase
-                                            placeholder="Search…"
-                                            {...inputProps}
-                                            // onChange={(event) => {
-                                            //     const value = event.target.value;
-                                            //     if (!value || !value.trim()) return
-                                            //     client.writeData({ data: { tickerName: value } })
-                                            // }}
-                                            classes={{
-                                                root: classes.inputRoot,
-                                                input: classes.inputInput,
-                                            }}
-
-                                        />
-                                    )}
-                                    renderSuggestionsContainer={options => {
-                                        return (
-                                            <div>
-                                                <Popper anchorEl={this.popperNode} open={Boolean(options.children)}>
-                                                    <Paper
-                                                        square
-                                                        {...options.containerProps}
-                                                        style={{ width: this.popperNode ? this.popperNode.clientWidth : null }}
-                                                    >
-                                                        {options.children}
-                                                    </Paper>
-                                                </Popper>
-                                            </div>
-
-                                        )
-                                    }}
-                                    theme={{
-                                        suggestionsList: classes.suggestionsList,
-                                        suggestion: classes.suggestion,
+                                return (
+                                    <MenuItem selected={isHighlighted} component="div">
+                                        <div>
+                                            {parts.map((part, index) =>
+                                                part.highlight ? (
+                                                    <span key={String(index)} style={{ fontWeight: 500 }}>
+                                                        {part.text}
+                                                    </span>
+                                                ) : (
+                                                        <strong key={String(index)} style={{ fontWeight: 300 }}>
+                                                            {part.text}
+                                                        </strong>
+                                                    ),
+                                            )}
+                                        </div>
+                                    </MenuItem>
+                                );
+                            }}
+                            renderInputComponent={(inputProps) => (
+                                <InputBase
+                                    placeholder="Search…"
+                                    {...inputProps}
+                                    // onChange={(event) => {
+                                    //     const value = event.target.value;
+                                    //     if (!value || !value.trim()) return
+                                    //     client.writeData({ data: { tickerName: value } })
+                                    // }}
+                                    classes={{
+                                        root: classes.inputRoot,
+                                        input: classes.inputInput,
                                     }}
 
                                 />
-                            )
-                        }
-                        }
-                    </ApolloConsumer>
-                )}
-            </Query>
+                            )}
+                            renderSuggestionsContainer={options => {
+                                return (
+                                    <div>
+                                        <Popper anchorEl={this.popperNode} open={Boolean(options.children)}>
+                                            <Paper
+                                                square
+                                                {...options.containerProps}
+                                                style={{ width: this.popperNode ? this.popperNode.clientWidth : null }}
+                                            >
+                                                {options.children}
+                                            </Paper>
+                                        </Popper>
+                                    </div>
+
+                                )
+                            }}
+                            theme={{
+                                suggestionsList: classes.suggestionsList,
+                                suggestion: classes.suggestion,
+                            }}
+
+                        />
+                    )
+                }
+                }
+            </ApolloConsumer>
+
 
         );
     }
